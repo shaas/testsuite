eb3113ce7ab64af61051e8c09b4946843ac8b15e
roland <roland>
2007-09-24 06:26:02 +0000

    RD-2007-09-24-0: Bugfix:    fixed check_qsub_ts_iz_145 test                  Review:    OJ

--- src/checktree/system_tests/clients/qsub/step.sh
+++ src/checktree/system_tests/clients/qsub/step.sh
@@ -4,11 +4,9 @@
 
 echo first $SGE_TASK_FIRST last $SGE_TASK_LAST step $SGE_TASK_STEPSIZE 
 
-sleep 11
 echo start is $SGE_TASK_ID
 
-start=$SGE_TASK_ID
-end=`expr $start + $SGE_TASK_STEPSIZE - 1`
+end=`expr $SGE_TASK_ID + $SGE_TASK_STEPSIZE - 1`
 
 echo first is $start
 echo last is $end
