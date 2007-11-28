fce964dad26cac05764ae3d94023efb261b15b00
jana <jana>
2007-11-28 12:15:36 +0000

    JO-2007-11-28-0: Enhancem.: Putback of view correction JO-2007-10-12-0

--- src/checktree_arco/checktree.tcl
+++ src/checktree_arco/checktree.tcl
@@ -96,7 +96,7 @@ set ARCO_VIEWS { view_job_times view_jobs_completed
 if {$ts_config(gridengine_version) >= 62} {
    set ARCO_VIEWS { view_ar_time_usage view_job_times view_jobs_completed
                  view_job_log view_department_values view_group_values view_host_values
-                 view_project_values view_queue_values view_user_values view_user_values
+                 view_project_values view_queue_values view_user_values view_statistic
                  view_ar_time_usage view_ar_attribute view_ar_log view_ar_usage 
                  view_ar_resource_usage view_accounting view_ar_attribute
    }
