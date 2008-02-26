80db8820df2e30c0b1355953592b532d9923a5ec
crei <crei>
2008-02-26 16:00:48 +0000

    CR-2008-02-26-1: Cleanup:   removed ts_log_fine cat output of                             wait_for_remote_file                  Review:    EB

--- src/tcl_files/file_procedures.tcl
+++ src/tcl_files/file_procedures.tcl
@@ -2771,8 +2771,8 @@ proc wait_for_remote_file { hostname user path { mytimeout 60 } {raise_error 1} 
       if {$to_go_away == 0} {
          ts_log_finer "ok - file exists on host $hostname"
          set output [start_remote_prog $hostname $user "cat" "$path" prg_exit_state 60 0 "" "" 0 0]
-         ts_log_fine "output of cat $path:\n$output"
          if {$prg_exit_state != 0} {
+            ts_log_finest "output of cat $path:\n$output"
             ts_log_fine "test says file exists, but cat does not work correctly! Touching file ..."
             start_remote_prog $hostname $user "touch" "$path" prg_exit_state 60 0 "" "" 0 0
          }
