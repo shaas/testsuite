2507d7fc8cd9e3fd5fc4e32b7bc918738fdf3fdf
roland <roland>
2008-10-27 10:38:35 +0000

    RD-2008-10-27-0: Enhancem.:  - Added test for IZ 2762                              - switched worker.sh to sleeper.sh in enforce_limits test                  Review:     CR

--- src/checktree/functional/enforce_limits/qrexec.sh
+++ src/checktree/functional/enforce_limits/qrexec.sh
@@ -7,7 +7,7 @@ echo "starting $NSLOTS at host `hostname`"
 I=1
 while [ $I -le $NSLOTS ]; do
    host=`sed -n "${I}p" $TMPDIR/machines`
-   cmd="$SGE_ROOT/bin/$ARC/qrsh -nostdin -inherit $host $SGE_ROOT/examples/jobs/worker.sh $1"
+   cmd="$SGE_ROOT/bin/$ARC/qrsh -nostdin -inherit $host $SGE_ROOT/examples/jobs/sleeper.sh $1"
    env
    ls -la $TMPDIR
    echo $cmd
