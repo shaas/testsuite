67f2d22a933b05d760f6ae69f449400cc6faee29
joga <joga>
2008-12-02 13:59:22 +0000

    JG-2008-12-02-0: Bugfix:     fixed 6.0 and 6.1 installation without jmx port set                  Review:     CR Issue number: Obtained from: Submitted by: Reviewed by:

--- src/checktree/install_core_system/automatic/qmaster.60.tcl
+++ src/checktree/install_core_system/automatic/qmaster.60.tcl
@@ -104,7 +104,7 @@ proc install_qmaster {} {
       append feature_install_options "-csp"
    }
 
-   if {$ts_config(jmx_port) == 0} {
+   if {$ts_config(jmx_port) == 0 && $ts_config(gridengine_version) >= 62} {
       append feature_install_options " -no-jmx"
    } elseif {$ts_config(jmx_port) > 0} {
       # For the JMX MBean Server we need java 1.5
