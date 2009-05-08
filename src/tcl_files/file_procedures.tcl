e90f8e0f495ca7367910897646e700648bf49120
crei <crei>
2009-05-08 15:20:12 +0000

    changed logging level from info to fine Issue number: Obtained from: Submitted by: Reviewed by:

--- src/tcl_files/file_procedures.tcl
+++ src/tcl_files/file_procedures.tcl
@@ -2653,7 +2653,7 @@ proc remote_delete_directory {hostname path {win_local_user 0}} {
       if {[have_root_passwd] == 0 && $CHECK_ADMIN_USER_SYSTEM == 0 } {
          # make sure we actually can delete the directory with all its contents.
          map_special_users $hostname $CHECK_USER $win_local_user
-         ts_log_info "doing chown -R $connect_full_user $path on $hostname as user root ..."
+         ts_log_fine "doing chown -R $connect_full_user $path on $hostname as user root ..."
          start_remote_prog $hostname "root" chown "-R $connect_full_user $path" prg_exit_state 60 0 "" "" 1 0 0 1 $win_local_user
       }
 
