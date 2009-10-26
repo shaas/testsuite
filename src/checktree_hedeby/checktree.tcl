320815557f9a9c5b6cc3044e66a7266947e69d0c
MarcinGoldyn <MarcinGoldyn>
2009-10-26 12:27:41 +0000

    TS MG-2009-10-26-0: Bugfix:now its possible to build cloud adapter with SDM TS

--- src/checktree_hedeby/checktree.tcl
+++ src/checktree_hedeby/checktree.tcl
@@ -415,7 +415,7 @@ proc create_testsuite_properties_file { property_path build_host } {
 
    puts $CHECK_OUTPUT "creating testsuite property_file ..."
    set date [clock format [clock seconds] -format "%d. %b %Y - %H:%M:%S"]
-   set data(0) 7
+   set data(0) 8
    set data(1) "# automatic generated build_testsuite.properties file from"
    set data(2) "# testsuite. ($date)"
    set data(3) "sge.root=$ts_config(product_root)" 
@@ -423,6 +423,7 @@ proc create_testsuite_properties_file { property_path build_host } {
    set data(5) "suppress.arch.check=true"
    set data(6) "#nfs.server=<enter name of nfs server host>"
    set data(7) "#remote.starter=rsh"
+   set data(8) "hedeby-cloud-adapter.dir=cloud-adapter"
    save_file $property_path data
    wait_for_remote_file $build_host $CHECK_USER $property_path
 
